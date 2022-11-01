6df8b5753456f705161d9a50cdd33c88135aa0e2
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-11-01 18:34:16 +0000

    Added write disp_speed function

--- src/files.py
+++ src/files.py
@@ -1,7 +1,9 @@
+import h5py
 import os
+from input import nx_c, ny_c, nz_c
 
 # File path
-path = '/hpcwork/itv/Antonio/premixed_jet_flames/'
+path = '/hpcwork/itv/Antonio/premixed_jet_flames'
 
 # Flames
 flames = ["R1K1", "R2K1", "R3K1", "R4K1"]
@@ -16,7 +18,8 @@ def list_data_files(flame):
     """Return lists of data files."""
 
     # List of files in directory
-    dir_list = os.listdir(path + flame + "/")
+    file_path = "%s/%s/" % (path, flame)
+    dir_list = os.listdir(file_path)
 
     # Initialise empty arrays
     data_file1_list = []
@@ -36,9 +39,29 @@ def list_data_files(flame):
     return [data_file1_list, data_file2_list]
 
 
+def write_disp_speed(prog_var, disp_speed, data_file, flame):
+    """
+    Write progress variable and displacement speed into reduced data files.
+    """
+
+    # Set file path
+    file_path = ".data/%s/%s_disp_speed.hdf5" % (flame, data_file)
+
+    # Open file
+    f1 = h5py.File(file_path, "w")
+
+    # Save dataset
+    f1.create_dataset("c_half", (nx_c, ny_c, nz_c), data=prog_var)
+    f1.create_dataset("s_d", (nx_c, ny_c, nz_c), data=disp_speed)
+
+    print("\nSaved progress variable and displacement speed!\n")
+
+
 # Driver function
 data_files = list_data_files(flames[0])
+data_files1 = data_files[0]
+data_files2 = data_files[1]
 
 # Print files
-print(data_files[0])
-print(data_files[1])
+print(data_files1)
+print(data_files2)
