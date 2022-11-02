e1e1e08578deea707582f69b6dd5a8df1247b0fa
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-11-02 14:34:06 +0000

    Updated duplicate variable names in main

--- src/main.py
+++ src/main.py
@@ -25,17 +25,17 @@ w_half = prog_var.calc_w(input.data_file1, input.data_file2,
                          input.ix_end, input.iy_end, input.iz_end)
 
 # Calculate progress variable
-prog_var = prog_var.calc_prog_var(input.data_file1, input.data_file2,
-                                  input.ix_start, input.iy_start,
-                                  input.iz_start, input.ix_end,
-                                  input.iy_end, input.iz_end)
+c = prog_var.calc_prog_var(input.data_file1, input.data_file2,
+                           input.ix_start, input.iy_start,
+                           input.iz_start, input.ix_end,
+                           input.iy_end, input.iz_end)
 
-c_half = prog_var[0]
-dc = prog_var[1]
+c_half = c[0]
+dc = c[1]
 
 # Calculate displacement speed
 print('Calculating displacement speed...')
-disp_speed = disp_speed.calc_disp_speed(u_half, v_half, w_half, c_half, dc)
+s_d = disp_speed.calc_disp_speed(u_half, v_half, w_half, c_half, dc)
 
 # Calculate strain rate tensor eigenvalues
 print('Calculating strain rate tensor eigenvalues...')
@@ -44,7 +44,7 @@ lambda_eig = strain_rate_tensor.calc_strain_rate_tensor_eig(input.if_save,
                                                             v_half,
                                                             w_half,
                                                             c_half,
-                                                            disp_speed)
+                                                            s_d)
 
 # Plot progress variable
 plot.plot_prog_var(c_half)
