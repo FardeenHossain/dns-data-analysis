7cff2591be0d1c65e4bbcde0ae5cf30ed8df4360
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-11-08 16:52:31 +0000

    Added calculate plot data function

--- src/calc_var.py
+++ src/calc_var.py
@@ -6,6 +6,8 @@ from input import ix_start, iy_start, iz_start, ix_end, iy_end, iz_end
 
 
 def calculate_variables(data_file1_path, data_file2_path):
+    """ Calculate displacement speed and strain rate tensor eigenvalues."""
+
     # Calculate U
     u_half = prog_var.calc_u(data_file1_path, data_file2_path, ix_start,
                              iy_start, iz_start, ix_end, iy_end, iz_end)
@@ -40,3 +42,16 @@ def calculate_variables(data_file1_path, data_file2_path):
     rr3 = lambda_eig[5]
 
     return [c_half, s_d, lambda1, lambda2, lambda3, rr1, rr2, rr3]
+
+
+def calculate_plot_data(lambda1, lambda2, lambda3, c_half, s_d):
+    """Calculate joint probability density function plot data."""
+
+    plot_data = strain_rate.calc_strain_rate_jpdf(lambda1, lambda2, lambda3,
+                                                  c_half, s_d)
+
+    return (plot_data[0], plot_data[1], plot_data[2], plot_data[3],
+            plot_data[4], plot_data[5], plot_data[6], plot_data[7],
+            plot_data[8], plot_data[9], plot_data[10], plot_data[11],
+            plot_data[12], plot_data[13], plot_data[14], plot_data[15],
+            plot_data[16])
