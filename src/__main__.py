9c025d698c41ab2dd60eb3b4444716449e9721fb
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-11-09 11:11:20 +0000

    Added read plot data to main

--- src/__main__.py
+++ src/__main__.py
@@ -16,10 +16,10 @@ if input.import_data == 1:
     # Read reduced data files
     reduced_data1 = files.read_disp_speed(input.data_file1)
     reduced_data2 = files.read_lambda(input.data_file1)
+    plot_data = files.read_plot_data(input.data_file1)
 
     c_half = reduced_data1[0]
     s_d = reduced_data1[1]
-
     lambda1 = reduced_data2[0]
     lambda2 = reduced_data2[1]
     lambda3 = reduced_data2[2]
@@ -31,21 +31,18 @@ else:
 
     c_half = data[0]
     s_d = data[1]
-
     lambda1 = data[2]
     lambda2 = data[3]
     lambda3 = data[4]
 
-# Calculate strain rate tensor joint probability density function
-lambda_jpdf = strain_rate.calc_strain_rate_jpdf(lambda1, lambda2, lambda3,
-                                                c_half, s_d)
+    plot_data = calc_var.calculate_plot_data(lambda1, lambda2, lambda3,
+                                             c_half, s_d)
 
 # Plot graphs
-plot.plot_all(c_half, s_d, lambda_jpdf[0], lambda_jpdf[1], lambda_jpdf[2],
-              lambda_jpdf[3], lambda_jpdf[4], lambda_jpdf[5], lambda_jpdf[6],
-              lambda_jpdf[7], lambda_jpdf[8], lambda_jpdf[9], lambda_jpdf[10],
-              lambda_jpdf[11], lambda_jpdf[12], lambda_jpdf[13],
-              lambda_jpdf[14])
+plot.plot_all(c_half, s_d, plot_data[0], plot_data[1], plot_data[2],
+              plot_data[3], plot_data[4], plot_data[5], plot_data[6],
+              plot_data[7], plot_data[8], plot_data[9], plot_data[10],
+              plot_data[11], plot_data[12], plot_data[13], plot_data[14])
 
 # Print finish
 print("\nFinished!")
