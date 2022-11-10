f12025ebd780a1f8deb6b144cb6ca281aac60f38
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-11-10 16:19:59 +0000

    Added PDF data to text files

--- src/files.py
+++ src/files.py
@@ -6,6 +6,85 @@ import calc_var
 from input import nx_c, ny_c, nz_c, in_path, flame
 
 
+def list_data_files():
+    """Return lists of data files."""
+
+    # List of files in directory
+    dir_list = os.listdir(in_path)
+
+    # Initialise empty arrays
+    data_file1_list = []
+    data_file2_list = []
+
+    for data_file in dir_list:
+        if data_file.startswith("data2"):
+            # Data file name
+            data_file1 = data_file.replace("data2", "data")
+            data_file2 = data_file
+
+            # Add to list
+            data_file1_list.append(data_file1)
+            data_file2_list.append(data_file2)
+
+    return [data_file1_list, data_file2_list]
+
+
+def write_data_files():
+    """
+    Writes reduced data files storing progress variable, displacement speed,
+    and strain rate tensor eigenvalues.
+    """
+
+    # List of data files
+    data_files = list_data_files()
+    data_files1 = data_files[0]
+    data_files2 = data_files[1]
+
+    for i in range(0, len(data_files1)):
+        # Print current data file
+        data_file = data_files1[i]
+        print('\nCalculating: %s ' % data_file)
+        print('\r----\n')
+
+        # Set data file path
+        data_file1_path = os.path.join(in_path, data_files1[i])
+        data_file2_path = os.path.join(in_path, data_files2[i])
+
+        # Calculate variables
+        var = calc_var.calculate_variables(data_file1_path, data_file2_path)
+
+        # Assign variables
+        c_half = var[0]
+        s_d = var[1]
+        lambda1 = var[2]
+        lambda2 = var[3]
+        lambda3 = var[4]
+        rr1 = var[5]
+        rr2 = var[6]
+        rr3 = var[7]
+
+        # Calculate PDF values
+        pdf = calc_var.calculate_pdf(c_half, s_d, lambda1, lambda2, lambda3)
+
+        # Assign variables
+        s_d_pdf = pdf[0]
+        s_d_bin_pdf = pdf[1]
+        lambda1_pdf = pdf[2]
+        lambda1_bin_pdf = pdf[3]
+        lambda2_pdf = pdf[4]
+        lambda2_bin_pdf = pdf[5]
+        lambda3_pdf = pdf[6]
+        lambda3_bin_pdf = pdf[7]
+
+        # Write data files
+        write_disp_speed(data_file, c_half, s_d)
+        write_lambda(data_file, lambda1, lambda2, lambda3, rr1, rr2, rr3)
+
+        # Write PDF data
+        write_pdf(data_file, c_half, s_d, s_d_pdf, lambda1, lambda1_pdf,
+                  lambda2, lambda2_pdf, lambda3, lambda3_pdf)
+
+
 def write_disp_speed(data_file, prog_var, disp_speed):
     """
     Write progress variable and displacement speed into reduced data files.
@@ -100,109 +179,33 @@ def read_lambda(data_file):
     return [lambda1, lambda2, lambda3, rr1, rr2, rr3]
 
 
-def list_data_files():
-    """Return lists of data files."""
+def write_pdf(data_file, c_half, s_d, s_d_pdf, lambda1, lambda1_pdf,
+              lambda2, lambda2_pdf, lambda3, lambda3_pdf):
+    """Write probability density function to text file."""
 
-    # List of files in directory
-    dir_list = os.listdir(in_path)
+    # Reformat data file name
+    data_file = data_file.replace(".h5", "")
 
-    # Initialise empty arrays
-    data_file1_list = []
-    data_file2_list = []
+    # Set file path
+    file_path = "./data/%s/%s_plot.txt" % (flame, data_file)
 
-    for data_file in dir_list:
-        if data_file.startswith("data2"):
-            # Data file name
-            data_file1 = data_file.replace("data2", "data")
-            data_file2 = data_file
+    # Open file
+    file = open(file_path, "w+")
 
-            # Add to list
-            data_file1_list.append(data_file1)
-            data_file2_list.append(data_file2)
+    for i in range(0, len(c_half[:, 0, 0])):
+        for j in range(0, len(c_half[0, :, 0])):
+            for k in range(0, len(c_half[0, 0, k])):
+                file.write("%d %d %d %d %d %d %d %d %d" %
+                           (c_half[i, j, k], s_d[i, j, k], s_d_pdf[i, j, k],
+                            lambda1[i, j, k], lambda1_pdf[i, j, k],
+                            lambda2[i, j, k], lambda2_pdf[i, j, k],
+                            lambda3[i, j, k], lambda3_pdf[i, j, k]))
 
-    return [data_file1_list, data_file2_list]
+    print("Saved strain rate tensor probability density function!")
 
+    # Close file
+    file.close()
 
-def write_data_files():
-    """
-    Writes reduced data files storing progress variable, displacement speed,
-    and strain rate tensor eigenvalues.
-    """
-
-    # List of data files
-    data_files = list_data_files()
-    data_files1 = data_files[0]
-    data_files2 = data_files[1]
-
-    for i in range(0, len(data_files1)):
-        # Print current data file
-        data_file = data_files1[i]
-        print('\nCalculating: %s ' % data_file)
-        print('\r----\n')
-
-        # Set data file path
-        data_file1_path = os.path.join(in_path, data_files1[i])
-        data_file2_path = os.path.join(in_path, data_files2[i])
-
-        # Calculate variables
-        var = calc_var.calculate_variables(data_file1_path, data_file2_path)
-
-        # Assign variables
-        c_half = var[0]
-        s_d = var[1]
-        lambda1 = var[2]
-        lambda2 = var[3]
-        lambda3 = var[4]
-        rr1 = var[5]
-        rr2 = var[6]
-        rr3 = var[7]
-
-        # Write reduced data files
-        write_disp_speed(data_file, c_half, s_d)
-        write_lambda(data_file, lambda1, lambda2, lambda3, rr1, rr2, rr3)
-
-# def write_plot_data(pdf_disp_speed_cond, bin_pdf_disp_speed_cond,
-#                     bin_disp_speed, lambda1_jpdf_bin,
-#                     lambda1_disp_speed_c_jpdf, lambda1_cond_mean,
-#                     disp1_jpdf_bin, lambda2_jpdf_bin,
-#                     lambda2_disp_speed_c_jpdf, lambda2_cond_mean,
-#                     disp2_jpdf_bin, lambda3_jpdf_bin,
-#                     lambda3_disp_speed_c_jpdf, lambda3_cond_mean,
-#                     disp3_jpdf_bin, data_file):
-#     """Write plot data to text file."""
-#
-#     # Reformat data file name
-#     data_file = data_file.replace(".h5", "")
-#
-#     # Set file path
-#     file_path = "./data/%s/%s_plot.txt" % (flame, data_file)
-#
-#     # Open file
-#     file = open(file_path, "w+")
-#
-#     # Write data
-#     file.write(str(pdf_disp_speed_cond) + "," +
-#                str(bin_pdf_disp_speed_cond) + "," +
-#                str(bin_disp_speed) + "," +
-#                str(lambda1_jpdf_bin) + "," +
-#                str(lambda1_disp_speed_c_jpdf) + "," +
-#                str(lambda1_cond_mean) + "," +
-#                str(disp1_jpdf_bin) + "," +
-#                str(lambda2_jpdf_bin) + "," +
-#                str(lambda2_disp_speed_c_jpdf) + "," +
-#                str(lambda2_cond_mean) + "," +
-#                str(disp2_jpdf_bin) + "," +
-#                str(lambda3_jpdf_bin) + "," +
-#                str(lambda3_disp_speed_c_jpdf) + "," +
-#                str(lambda3_cond_mean) + "," +
-#                str(disp3_jpdf_bin))
-#
-#     print("Saved strain rate tensor joint probability density function!")
-#
-#     # Close file
-#     file.close()
-#
-#
 # def read_plot_data(data_file):
 #     """Read plot data from text file."""
 #
