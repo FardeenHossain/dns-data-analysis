257d49e53700ad2b2f3802b2e13594a8ab454ddb
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-11-14 10:52:38 +0000

    Added test PDF plots

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,7 @@
 import input
 import files
 import calc_var
+import matplotlib.pyplot as plt
 
 print('\nDirect Numerical Simulation (DNS) Premixed')
 print('\r----\n')
@@ -34,6 +35,31 @@ files.write_pdf(input.data_file1, lambda1_pdf, lambda1_pdf_bin, lambda2_pdf,
 print(s_d_pdf)
 print(lambda1_pdf)
 print(lambda1_jpdf)
+
+plt.plot(s_d_pdf_bin, s_d_pdf[0, :])
+plt.ylabel('Probability Density Function, PDF')
+plt.xlabel(r'Displacement Speed, $\rmS_{d}$')
+plt.xlim(-15, 15)
+plt.ylim(0.0, 0.1)
+plt.show()
+
+plt.plot(lambda1_pdf_bin, lambda1_pdf_bin[0, :])
+plt.ylabel('Probability Density Function, PDF')
+plt.xlabel(r'Compressive Strain Rate Tensor, $\rm\gamma$')
+plt.xlim(-5e5, 5e5)
+plt.ylim(0.0, 2e-5)
+plt.show()
+
+plt.contourf(lambda1_jpdf_bin_y, lambda1_jpdf_bin_x, lambda1_jpdf,
+             cmap='inferno')
+plt.xlabel(r'Displacement Speed, $\rmS_{d}$')
+plt.ylabel(r'Compressive Strain Rate Tensor, $\rm\gamma$')
+plt.colorbar(label='Joint Probability Density Function, JPDF')
+plt.legend(loc='upper right', labelcolor='white')
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+plt.xlim(-15, 15)
+plt.ylim(-1.5e5, 1.5e5)
+plt.show()
 
 print("\nFinished!")
 print("\r----\n")
