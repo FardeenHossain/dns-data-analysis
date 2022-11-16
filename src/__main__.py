bb0223ac82a9b85a0d7e00aa81cbaba7d32a8657
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-11-16 18:28:30 +0000

    Added dividers to prints

--- src/__main__.py
+++ src/__main__.py
@@ -5,15 +5,17 @@ print("\nDNS Data Analysis")
 print("\r----\n")
 
 if input.write_reduced_data == 1:
-    print("\nWriting reduced data...\n")
+    print("\nWriting reduced data...")
+    print("\r----\n")
     files.write_reduced_data()
 
 if input.write_plot_data == 1:
-    print("\nWriting plot data...\n")
+    print("\nWriting plot data...")
+    print("\r----\n")
     files.write_plot_data()
 
 if input.write_reduced_data == 0 & input.write_plot_data == 0:
-    print("\nNo input arguments!\n")
+    print("No input arguments!")
 
 print("\nFinished!")
 print("\r----\n")
