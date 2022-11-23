bd74c6fd45b41e666daeb71794b73b616f259c9b
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-11-23 16:37:32 +0000

    Added test function to plot progress variable

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,11 @@
 import input
 import files
 
+import os
+import prog_var
+from input import in_path, ix_start, iy_start, iz_start, ix_end, iy_end, iz_end
+import matplotlib.pyplot as plt
+
 print("\nDNS Data Analysis")
 print("\r----\n")
 
@@ -14,6 +19,19 @@ if input.write_plot_data == 1:
 
 if input.write_reduced_data == 0 & input.write_plot_data == 0:
     print("No input arguments!\n")
+
+# PLOT PROGRESS VARIABLE
+[data_file1_list, data_file2_list] = files.list_data_files()
+data_file1_path = os.path.join(in_path, data_file1_list[0])
+data_file2_path = os.path.join(in_path, data_file2_list[0])
+[c_half, dc] = prog_var.calc_prog_var(data_file1_path, data_file2_path,
+                                      ix_start, iy_start, iz_start, ix_end,
+                                      iy_end, iz_end)
+plt.contourf(c_half[:, :, 0], cmap='plasma')
+plt.xlabel('Y-Coordinate')
+plt.ylabel('X-Coordinate')
+plt.colorbar(label='Progress Variable, C (-)')
+plt.show()
 
 print("\nFinished!")
 print("\r----\n")
--- src/input.py
+++ src/input.py
@@ -2,19 +2,32 @@ import os
 
 flames = ["R1K1", "R2K1", "R3K1", "R4K1"]
 
-i = 2               # 0 = R1K1, 1 = R2K1, 2 = R3K1, 3 = R4K1
+i = 0               # 0 = R1K1, 1 = R2K1, 2 = R3K1, 3 = R4K1
 flame = flames[i]   # Select flame from array
 
 in_path = f"/hpcwork/itv/Antonio/premixed_jet_flames/{flame}/"
 
 # Input arguments
-write_reduced_data = 1
-write_plot_data = 1
+write_reduced_data = 0
+write_plot_data = 0
 
 # Data size array
 nx_array = [720, 1440, 2880, 5760]
 ny_array = [480, 960, 1922, 3844]
 nz_array = [256, 256, 512, 1024]
+
+# Chunk size array
+# nx_c_array = [20, 20, 20, 20]
+# ny_c_array = [100, 100, 100, 100]
+# nz_c_array = [255, 255, 511, 1023]
+nx_c_array = [719, 1339, 2879, 5759]
+ny_c_array = [479, 959, 1921, 3843]
+nz_c_array = [255, 255, 511, 1023]
+
+# Start point array
+ix_start_array = [200, 400, 800, 1000]
+iy_start_array = [200, 400, 850, 1750]
+iz_start_array = [0, 0, 0, 0]
 
 # Data size
 nx = nx_array[i]
@@ -22,19 +35,17 @@ ny = ny_array[i]
 nz = nz_array[i]
 
 # Chunk size
-nx_c = 100
-ny_c = 100
-nz_c = 10
-
-# Start point array
-ix_start_array = [200, 400, 800, 1000]
-iy_start_array = [200, 400, 850, 1750]
-iz_start_array = [1, 1, 1, 1]
+nx_c = nx_c_array[i]
+ny_c = ny_c_array[i]
+nz_c = nz_c_array[i]
 
 # Start point
-ix_start = ix_start_array[i]
-iy_start = iy_start_array[i]
-iz_start = iz_start_array[i]
+# ix_start = ix_start_array[i]
+# iy_start = iy_start_array[i]
+# iz_start = iz_start_array[i]
+ix_start = 0
+iy_start = 0
+iz_start = 0
 
 # End point
 ix_end = ix_start + nx_c
