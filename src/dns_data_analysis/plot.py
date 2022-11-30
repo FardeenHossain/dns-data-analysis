4713df57c900cd6c4faef512b39e24886021249e
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-11-30 20:49:33 +0000

    Added contour limit

--- src/dns_data_analysis/plot.py
+++ src/dns_data_analysis/plot.py
@@ -14,10 +14,10 @@ def plot_flame():
 def plot_prog_var(c_half):
     """Contour plot of progress variable."""
 
-    plt.contourf(c_half[:, :, 0], cmap='plasma')
-    plt.xlabel('Y-Coordinate')
-    plt.ylabel('X-Coordinate')
-    plt.colorbar(label='Progress Variable, C (-)')
+    plt.contourf(c_half[:, :, 0], cmap='plasma', vmin=0, vmax=1)
+    plt.xlabel('y')
+    plt.ylabel('x')
+    plt.colorbar(label='C')
     plt.show()
 
 
