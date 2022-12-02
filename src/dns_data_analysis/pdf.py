9cd51581883e5a9cc9f6c0bec660fd0bb71b8150
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-12-02 10:31:28 +0000

    Added conditional mean functions

--- src/dns_data_analysis/pdf.py
+++ src/dns_data_analysis/pdf.py
@@ -82,3 +82,50 @@ def calc_strain_rate_jpdf(c_half, s_d, lambda1, lambda2, lambda3):
     return [lambda1_jpdf, lambda1_jpdf_bin_x, lambda1_jpdf_bin_y, lambda2_jpdf,
             lambda2_jpdf_bin_x, lambda2_jpdf_bin_y, lambda3_jpdf,
             lambda3_jpdf_bin_x, lambda3_jpdf_bin_y]
+
+
+def calc_cond_mean(c_half, s_d, lambda1, lambda2, lambda3):
+    """Calculate conditional mean of displacement speed and """
+
+    bin_c_cond = np.linspace(0.1, 0.9, 5)
+    d_bin_c_cond = 0.01
+    nc = len(bin_c_cond)
+
+    # Condition
+    for j in range(0, nc):
+        cond = np.absolute(c_half - bin_c_cond[j]) < d_bin_c_cond / 2.0
+
+    # Extract condition
+    s_d_cond = np.extract(cond, s_d)
+    lambda1_cond = np.extract(cond, lambda1)
+    lambda2_cond = np.extract(cond, lambda2)
+    lambda3_cond = np.extract(cond, lambda3)
+
+    # Flatten array
+    s_d_cond_flat = np.ndarray.flatten(s_d_cond)
+    lambda1_flat = np.ndarray.flatten(lambda1_cond)
+    lambda2_flat = np.ndarray.flatten(lambda2_cond)
+    lambda3_flat = np.ndarray.flatten(lambda3_cond)
+
+    bin_s_d = np.linspace(-9, 14, 20)
+    d_bin_s_d = 1 / 20
+    nc = len(bin_s_d)
+
+    # Initialise arrays with zeros
+    lambda1_cond_mean = np.zeros([nc])
+    lambda2_cond_mean = np.zeros([nc])
+    lambda3_cond_mean = np.zeros([nc])
+
+    for i in range(0, nc):
+        cond = np.absolute(s_d_cond_flat - bin_s_d[i]) < d_bin_s_d / 2.0
+
+        lambda1_extract = np.extract(cond, lambda1_flat)
+        lambda1_cond_mean[i] = np.mean(lambda1_extract)
+
+        lambda2_extract = np.extract(cond, lambda2_flat)
+        lambda2_cond_mean[i] = np.mean(lambda2_extract)
+
+        lambda3_extract = np.extract(cond, lambda3_flat)
+        lambda3_cond_mean[i] = np.mean(lambda3_extract)
+
+    return [bin_s_d, lambda1_cond_mean, lambda2_cond_mean, lambda3_cond_mean]
