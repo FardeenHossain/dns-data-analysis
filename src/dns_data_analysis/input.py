a524337a9885934dbb7666923027ce3bd991ce95
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-12-06 16:40:40 +0100

    Fixed R3K1 bottom data

--- src/dns_data_analysis/input.py
+++ src/dns_data_analysis/input.py
@@ -5,7 +5,7 @@ plot_flame = 0
 full_data_range = 0
 
 i = 2   # 0 = R1K1, 1 = R2K1, 2 = R3K1, 3 = R4K1
-j = 1   # 0 = bot, 1 = mid, 2 = top
+j = 0   # 0 = bot, 1 = mid, 2 = top
 
 flames = ["R1K1", "R2K1", "R3K1", "R4K1"]
 positions = ['bot', 'mid', 'top']
