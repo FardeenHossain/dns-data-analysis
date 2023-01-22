39aeecf4f68fb4ed6554a7d24e786de54b3b777f
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2023-01-22 16:28:25 +0000

    Fixed bug in main for loop

--- src/dns_data_analysis/__main__.py
+++ src/dns_data_analysis/__main__.py
@@ -5,11 +5,11 @@ import plot
 print("\nDNS Data Analysis")
 print("\r----\n")
 
-print(f"Flame: {input.flame}")
-print(f"Position: {input.position}\n")
-
 
 def main():
+    print(f"Flame: {input.flame}")
+    print(f"Position: {input.position}\n")
+
     if input.write_reduced_data:
         print("Writing reduced data...\n")
         files.write_reduced_data()
@@ -24,8 +24,8 @@ def main():
 
 
 if input.flames_all:
-    for i in input.flames:
-        for j in input.positions:
+    for i in range(0, len(input.flames)):
+        for j in range(0, len(input.positions)):
             input.flame = input.flames[i]
             input.position = input.positions[j]
             main()
