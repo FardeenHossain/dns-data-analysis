94916b62546c1731ee0c65eaba108a3a553cb964
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2023-02-23 15:24:59 +0000

    Added disp speed reactive JPDF

--- src/dns_data_analysis/prod_rate.py
+++ src/dns_data_analysis/prod_rate.py
@@ -10,22 +10,22 @@ def main():
     print("\r----\n")
 
     print("Reading data...\n")
-
-    [prod_rate, s_d, c_half] = read_prod_rate()
+    [prod_rate, s_d, c_half, s_d_r] = read_prod_rate()
 
     print("Calculating data...\n")
-
     [prod_rate_pdf, prod_rate_bin_pdf] = calc_prod_rate_pdf(c_half, prod_rate)
 
     [prod_rate_cond_jpdf, prod_rate_cond_jpdf_bin_x,
      prod_rate_cond_jpdf_bin_y] = calc_prod_rate_cond_jpdf(c_half, s_d,
                                                            prod_rate)
 
+    [s_d_cond_jpdf, s_d_cond_jpdf_bin_x,
+     s_d_cond_jpdf_bin_y] = calc_disp_speed_cond_jpdf(c_half, s_d, s_d_r)
+
     [prod_rate_jpdf, prod_rate_jpdf_bin_x,
      prod_rate_jpdf_bin_y] = calc_prod_rate_prog_var_jpdf(c_half, prod_rate)
 
     print("Writing data...\n")
-
     write_prod_rate_pdf(prod_rate_pdf, prod_rate_bin_pdf)
 
     write_prod_rate_prog_var_jpdf(prod_rate_jpdf,
@@ -51,23 +51,35 @@ def read_prod_rate():
     f2 = h5py.File(disp_speed_file_path, "r")
 
     # Read variables
-    prod_rate_transpose = np.array(f1["data/source_O2"])
+    source_o2 = np.array(f1["data/source_O2"])
     s_d = np.array(f2["s_d"])
     c_half = np.array(f2["c_half"])
+    mag_g_c = np.array(f2["mag_g_c"])
+    rho_half = np.array(f2["rho_half"])
 
     # Initialise arrays with zeroes
-    prod_rate = np.zeros([len(prod_rate_transpose[0, 0, :]),
-                          len(prod_rate_transpose[0, :, 0]),
-                          len(prod_rate_transpose[:, 0, 0])])
+    prod_rate = np.zeros([len(source_o2[0, 0, :]),
+                          len(source_o2[0, :, 0]),
+                          len(source_o2[:, 0, 0])])
 
-    # Transpose array
-    for i in range(0, len(prod_rate_transpose[:, 0, 0])):
-        for j in range(0, len(prod_rate_transpose[i, :, 0])):
-            for k in range(0, len(prod_rate_transpose[i, j, :])):
-                prod_rate[k, j, i] = - (prod_rate_transpose[i, j, k] /
-                                        (o2_u - o2_b))
+    s_d_r = np.zeros([len(source_o2[0, 0, :]),
+                      len(source_o2[0, :, 0]),
+                      len(source_o2[:, 0, 0])])
 
-    return [prod_rate, s_d, c_half]
+    # Transpose array and calculate omega
+    for i in range(0, len(source_o2[:, 0, 0])):
+        for j in range(0, len(source_o2[i, :, 0])):
+            for k in range(0, len(source_o2[i, j, :])):
+                prod_rate[k, j, i] = - (source_o2[i, j, k] / (o2_u - o2_b))
+
+    # Calculate reactive displacement speed
+    for i in prod_rate[:, 0, 0]:
+        for j in prod_rate[i, :, 0]:
+            for k in prod_rate[i, j, :]:
+                s_d_r[i, j, k] = (prod_rate[i, j, k] /
+                                  rho_half[i, j, k]) / mag_g_c[i, j, k]
+
+    return [prod_rate, s_d, c_half, s_d_r]
 
 
 def calc_prod_rate_pdf(c_half, prod_rate):
@@ -118,6 +130,24 @@ def calc_prod_rate_cond_jpdf(c_half, s_d, prod_rate):
 
     return [prod_rate_cond_jpdf, prod_rate_cond_jpdf_bin_x,
             prod_rate_cond_jpdf_bin_y]
+
+
+def calc_disp_speed_cond_jpdf(c_half, s_d, s_d_r):
+    # Bin spacing
+    s_d_bin_edges_pdf = np.linspace(-1e2, 1e2, 200)
+    s_d_r_bin_edges_pdf = np.linspace(-1e2, 1e2, 200)
+
+    bin_c_cond = [0.1, 0.3, 0.5, 0.73, 0.9]
+    d_bin_c_cond = 0.1
+
+    # Calculate JPDF
+    [s_d_cond_jpdf, s_d_cond_jpdf_bin_x,
+     s_d_cond_jpdf_bin_y] = mystat.cond_pdf2d(s_d_r, s_d, c_half,
+                                              s_d_r_bin_edges_pdf,
+                                              s_d_bin_edges_pdf,
+                                              bin_c_cond, d_bin_c_cond)
+
+    return [s_d_cond_jpdf, s_d_cond_jpdf_bin_x, s_d_cond_jpdf_bin_y]
 
 
 def write_prod_rate_pdf(prod_rate_pdf, prod_rate_pdf_bin):
@@ -174,6 +204,27 @@ def write_prod_rate_cond_jpdf(prod_rate_jpdf, prod_rate_jpdf_bin_x,
                 file.write(
                     f"{prod_rate_jpdf_bin_x[j]} {prod_rate_jpdf_bin_y[k]} "
                     f"{prod_rate_jpdf[i, j, k]}\n")
+        file.write("\n")
+
+    file.close()
+
+
+def write_disp_speed_cond_jpdf(s_d_cond_jpdf, s_d_cond_jpdf_bin_x,
+                               s_d_cond_jpdf_bin_y):
+    data_file_path = "plots/R3K1_mid_jpdf_disp_speed_reactive.txt"
+    file_path = os.path.join(data_path, data_file_path)
+    file = open(file_path, "w+")
+
+    # Write headings
+    file.write("s_d_jpdf_bin_x s_d_jpdf_bin_y s_d_cond_jpdf\n")
+
+    # Write JPDF
+    for i in range(0, len(s_d_cond_jpdf[:, 0, 0])):
+        for j in range(0, len(s_d_cond_jpdf[i, :, 0])):
+            for k in range(0, len(s_d_cond_jpdf[i, j, :])):
+                file.write(
+                    f"{s_d_cond_jpdf_bin_x[j]} {s_d_cond_jpdf_bin_y[k]} "
+                    f"{s_d_cond_jpdf[i, j, k]}\n")
         file.write("\n")
 
     file.close()
---
+++ src/plots/disp_speed_reactive_JPDF/disp_speed_reactive_JPDF.gnu
@@ -0,0 +1,16 @@
+set terminal epslatex size 4,3 standalone color colortext 10
+set output 'disp_speed_reactive_JPDF.tex'
+
+f1 = '../../../data/plots/R3K1_mid_jpdf_disp_speed_reactive.txt'
+
+set ylabel '$S_d$'
+set xlabel '$S_d^R$'
+
+set xrange[-60:60]
+set yrange[-60:60]
+
+set format cb '%.1e'
+
+set palette defined ( 0 'white', 1 'blue', 2 'black')
+              
+plot f1 using 1:2:3 every :::3::3 title '' with image
