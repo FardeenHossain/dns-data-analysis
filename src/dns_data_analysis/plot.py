bc8c19e4149703d7a9d27e3269e10918d55b2a54
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2023-03-14 16:41:35 +0000

    Removed read plot data

--- src/dns_data_analysis/plot.py
+++ src/dns_data_analysis/plot.py
@@ -11,8 +11,7 @@ from input import in_path, data_path, dx, o2_u, o2_b, ix_start, iy_start, \
     iz_start, ix_end, iy_end, iz_end
 
 # Input arguments
-read = False
-calculate = False
+plot = False
 export = True
 reaction = False
 
@@ -21,22 +20,15 @@ def main():
     print("\nDNS Data Analysis - Plot Flame")
     print("\r----\n")
 
-    if read:
-        print("Reading data...\n")
-        [s_d, c_half] = read_disp_speed()
+    if plot:
+        print("Plotting data...\n")
+        [c_half, s_d, k] = calc_plot_data()
         plot_prog_var(c_half)
-        plot_disp_speed(s_d)
-        plot_cond_disp_speed(s_d, c_half)
 
     if export:
         print("Exporting data...\n")
         [c_half, s_d, k] = calc_plot_data()
         export_vtk(c_half, s_d, k)
-
-    if calculate:
-        print("Calculating data...\n")
-        [c_half, s_d, k] = calc_plot_data()
-        plot_prog_var(c_half)
 
     print("\nFinished!")
     print("\r----\n")
@@ -57,14 +49,13 @@ def calc_plot_data():
     data_file1_path = os.path.join(in_path, data_file1_list[0])
     data_file2_path = os.path.join(in_path, data_file2_list[0])
 
-    ix_start = 500
-    ix_end = 2500
-
-    iy_start = 1600
-    iy_end = 2200
-
-    iz_start = 0
-    iz_end = 8
+    # VTK grid size
+    # ix_start = 500
+    # ix_end = 2500
+    # iy_start = 1600
+    # iy_end = 2200
+    # iz_start = 0
+    # iz_end = 8
 
     u_half = prog_var.calc_u(data_file1_path, data_file2_path, ix_start,
                              iy_start, iz_start, ix_end, iy_end, iz_end)
@@ -82,23 +73,9 @@ def calc_plot_data():
     [s_d, mag_g_c] = disp_speed.calc_disp_speed(u_half, v_half, w_half, c_half,
                                                 dc)
 
-    k = curvature.mean_curv(c_half, dx)
+    k_m = curvature.mean_curv(c_half, dx)
 
-    return [c_half, s_d, k]
-
-
-def read_disp_speed():
-    """Read displacement speed from reduced data files."""
-
-    data_file_path = "R3K1/mid/data_1.800E-03_disp_speed.h5"
-    file_path = os.path.join(data_path, data_file_path)
-
-    f1 = h5py.File(file_path, "r")
-
-    c_half = np.array(f1["c_half"])
-    s_d = np.array(f1["s_d"])
-
-    return [s_d, c_half]
+    return [c_half, s_d, k_m]
 
 
 def read_prod_rate():
@@ -125,7 +102,7 @@ def read_prod_rate():
     return prod_rate
 
 
-def export_vtk(c_half, s_d, k):
+def export_vtk(c_half, s_d, k_m):
     nx = len(c_half[:, 0, 0])
     ny = len(c_half[0, :, 0])
     nz = len(c_half[0, 0, :])
@@ -140,11 +117,11 @@ def export_vtk(c_half, s_d, k):
         omega_c = read_prod_rate()
         gridToVTK("./output", x, y, z,
                   cellData={"c": c_half[:, :, :], "s_d": s_d[:, :, :],
-                            "k": k[:, :, :], "omega_c": omega_c[:, :, :]})
+                            "k_m": k_m[:, :, :], "omega_c": omega_c[:, :, :]})
     else:
         gridToVTK("./output", x, y, z,
                   cellData={"c": c_half[:, :, :], "s_d": s_d[:, :, :],
-                            "k": k[:, :, :]})
+                            "k_m": k_m[:, :, :]})
 
 
 if __name__ == "__main__":
